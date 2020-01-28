1ab8d55c818bd39ea245917c924882047e803cd1
Stefano Fancello <stefano.fancello@nethesis.it>
2020-01-28 10:50:01 +0100

    Ass scopeType and scopeId metadata to scope variables

--- src/Entity/Scope.php
+++ src/Entity/Scope.php
@@ -68,6 +68,11 @@ class Scope {
        $parents_vars = $this->getParentsVariables();
        $var_arrays[] = $parents_vars;
        $var_arrays[] = $this->data;
+       // Add metadata informations
+       $var_arrays[] = array(
+           'scopeType' => $this->metadata['scopeType'],
+           'scopeId' => $this->id,
+           );
        return call_user_func_array('array_merge', $var_arrays);
     }
 
