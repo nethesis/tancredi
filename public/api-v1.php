eba4dfae3e07f14d5013df91a6d881f723f03f10
Stefano Fancello <stefano.fancello@nethesis.it>
2020-05-12 17:09:43 +0200

    better file check

--- public/api-v1.php
+++ public/api-v1.php
@@ -419,8 +419,7 @@ $app->patch('/defaults', function (Request $request, Response $response, $args) 
 $app->post('/firmware', function(Request $request, Response $response) use ($app) {
     $uploadedFile = array_pop($request->getUploadedFiles());
     if ($uploadedFile->getError() === UPLOAD_ERR_OK) {
-        if (! preg_match('/^[a-zA-Z0-9\-_\.\(\)]+$/', $uploadedFile->getClientFilename())) {
-            $response = $response->withStatus(400);
+        if (! preg_match('/^[a-zA-Z0-9\-_\.()]+$/', $uploadedFile->getClientFilename())) {
             $results = array(
                 'type' => 'https://github.com/nethesis/tancredi/wiki/problems#malformed-data',
                 'title' => 'Invalid file name'
@@ -430,12 +429,12 @@ $app->post('/firmware', function(Request $request, Response $response) use ($app
             $response = $response->withHeader('Content-Language', 'en');
             return $response;
         }
+        $uploadedFile->moveTo($this->config['rw_dir'] . 'firmware' . '/' . $uploadedFile->getClientFilename());
         $realfile = realpath($this->config['rw_dir'] . 'firmware' . '/' . $uploadedFile->getClientFilename());
-        if( ! $realfile || dirname($realfile) != ($config['rw_dir'] . 'firmware')) {
+        if( ! $realfile || dirname($realfile) != ($this->config['rw_dir'] . 'firmware')) {
             // File not found
             return $response->withStatus(404);
         }
-        $uploadedFile->moveTo($realfile);
         return $response->withStatus(204);
     }
     return $response->withStatus(500);
@@ -465,21 +464,12 @@ $app->get('/firmware', function(Request $request, Response $response) use ($app)
 $app->delete('/firmware/{file}', function(Request $request, Response $response, $args) use ($app) {
     $file = $args['file'];
     $realfile = realpath($this->config['rw_dir'] . 'firmware' . '/' . $file);
-    if( ! $realfile ) {
+    if( ! $realfile  || dirname($realfile) != ($this->config['rw_dir'] . 'firmware')) {
         $results = array(
             'type' => 'https://github.com/nethesis/tancredi/wiki/problems#not-found',
             'title' => 'Resource not found'
         );
         $response = $response->withJson($results,404,JSON_FLAGS);
-        $response = $response->withHeader('Content-Type', 'application/problem+json');
-        $response = $response->withHeader('Content-Language', 'en');
-        return $response;
-    } elseif( dirname($realfile) != ($config['rw_dir'] . 'firmware')) {
-        $results = array(
-            'type' => 'https://github.com/nethesis/tancredi/wiki/problems#malformed-data',
-            'title' => 'Wrong file name'
-        );
-        $response = $response->withJson($results,400,JSON_FLAGS);
         $response = $response->withHeader('Content-Type', 'application/problem+json');
         $response = $response->withHeader('Content-Language', 'en');
         return $response;
