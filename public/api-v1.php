6bbe95a35d8be71cc53365ba22a8da7d8064c60f
Stefano <stefano.fancello@nethesis.it>
2020-05-14 15:13:12 +0200

    Update public/api-v1.php
    Co-authored-by: Davide Principi <davide.principi@nethesis.it>

--- public/api-v1.php
+++ public/api-v1.php
@@ -158,7 +158,7 @@ $app->patch('/phones/{mac}', function (Request $request, Response $response, $ar
     if (array_key_exists('variables',$patch_data)) {
         $scope = new \Tancredi\Entity\Scope($mac, $this->storage, $this->logger);
         $scope->setVariables($patch_data['variables']);
-        $response = $response->withStatus(200);
+        $response = $response->withJson(\Tancredi\Entity\Scope::getPhoneScope($mac, $this->storage, $this->logger), 200, JSON_FLAGS);
         return $response;
     }
     $response = $response->withStatus(400);
