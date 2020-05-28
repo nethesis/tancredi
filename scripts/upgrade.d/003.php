7f49f4fe6d2f94015c37a31028810983f4eb4dc8
Davide Principi <davide.principi@nethesis.it>
2020-05-28 18:29:52 +0200

    Fix Fanvil cap_ringtone_blacklist value

---
+++ scripts/upgrade.d/003.php
@@ -0,0 +1,37 @@
+<?php namespace upgrade3;
+
+/*
+ * Copyright (C) 2020 Nethesis S.r.l.
+ * http://www.nethesis.it - nethserver@nethesis.it
+ *
+ * This script is part of NethServer.
+ *
+ * NethServer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License,
+ * or any later version.
+ *
+ * NethServer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NethServer.  If not, see COPYING.
+ */
+
+$models = $container['storage']->listScopes('model');
+foreach ($models as $id) {
+    if(substr($id, 0, 6) != 'fanvil') {
+        continue;
+    } 
+    $scope = new \Tancredi\Entity\Scope($id, $container['storage'], $container['logger']);
+    if($scope->metadata['version'] >= 3) {
+        continue;
+    }
+    $scope->metadata['version'] = 3;
+    $scope->setVariables([
+        'cap_ringtone_blacklist' => '-1',
+    ]);
+    $container['logger']->info("Fixed cap_ringtone_blacklist for model $id");
+}
