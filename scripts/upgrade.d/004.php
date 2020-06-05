58b230a468529388e6305bd3c3a803d1db1b5195
Davide Principi <davide.principi@nethesis.it>
2020-06-05 18:22:05 +0200

    Upgrade scopes for screensaver and background file

---
+++ scripts/upgrade.d/004.php
@@ -0,0 +1,51 @@
+<?php namespace upgrade4;
+
+/*
+ * Copyright (C) 2020 Nethesis S.r.l.
+ * http://www.nethesis.it - nethserver@nethesis.it
+ *
+ * This script is part of NethServer.
+ *
+ * NethServer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License,
+ * or any later version.
+ *
+ * NethServer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NethServer.  If not, see COPYING.
+ */
+
+$models = $container['storage']->listScopes('model');
+foreach ($models as $id) {
+    if(substr($id, 0, 6) == 'fanvil') {
+        $scope = new \Tancredi\Entity\Scope($id, $container['storage'], $container['logger']);
+        if($scope->metadata['version'] >= 4) {
+            continue;
+        }
+        $scope->metadata['version'] = 4;
+        $scope->setVariables([
+            'cap_background_file' => '1',
+            'cap_screensaver_file' => '',
+        ]);
+        $container['logger']->info("Fixed cap_background_file, cap_screensaver_file in model $id");
+    }
+}
+
+$defaults = new \Tancredi\Entity\Scope('defaults', $container['storage'], $container['logger']);
+if($defaults->metadata['version'] < 4) {
+    $defaults->metadata['version'] = 4;
+    $defaults->setVariables([
+        'cap_ringtone_count' => "",
+        'cap_ringtone_blacklist' => "",
+        'background_file' => "",
+        'screensaver_file' => "",
+        'cap_background_file' => "",
+        'cap_screensaver_file' => "",
+    ]);
+    $container['logger']->info("Fixed screensaver, ringtone and background variables in defaults scope");
+}
