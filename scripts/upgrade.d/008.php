b5b45b2f7cb2fb3a0e672fb0454b0e5ae235e7e5
Stefano Fancello <stefano.fancello@nethesis.it>
2020-12-11 12:22:28 +0100

    Add upgrade script

---
+++ scripts/upgrade.d/008.php
@@ -0,0 +1,38 @@
+<?php namespace upgrade7;
+
+/*
+ * Copyright (C) 2020 Nethesis S.r.l.
+ * http://www.nethesis.it - nethserver@nethesis.it
+ *
+ * This script is part of NethServer.
+ *
+ * NethServer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License,
+ * or any later version.
+ *
+ * NethServer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NethServer.  If not, see COPYING.
+ */
+
+//
+// Set right gigaset Maxwell3 screensaver times blacklist
+//
+$fixes = [
+    'gigaset-Maxwell3'  => ['cap_screensaver_time_blacklist' => '3,5,7,10,15,30,60,120,300'],
+];
+
+foreach ($fixes as $model_id => $variables) {
+    $scope = new \Tancredi\Entity\Scope($model_id, $container['storage'], $container['logger']);
+    if(isset($scope->metadata['version']) && $scope->metadata['version'] >= 8) {
+        continue;
+    }
+    $scope->metadata['version'] = 8;
+    $scope->setVariables($variables);
+    $container['logger']->info("Fixed cap_linekey_count in model $model_id");
+}
