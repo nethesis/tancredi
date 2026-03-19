45b562517efb87d4f6d467f4aad1c72b29a67db5
Stefano Fancello <gentoo.stefano@gmail.com>
2026-03-19 11:28:42 +0100

    fix(upgrade): Update NPX5v2 firmware template
    Add an upgrade step that sets the NPX5v2 firmware template to
    nethesis-firmware-v2.tmpl.
    
    Bump the scope metadata version to 13 so existing installations
    receive the corrected template during upgrade.

---
+++ scripts/upgrade.d/013.php
@@ -0,0 +1,19 @@
+<?php namespace upgrade13;
+
+/*
+Update firmware template for NPX5v2
+*/
+
+$fixes = [
+    'nethesis-NPX5v2' => ['tmpl_firmware' => 'nethesis-firmware-v2.tmpl'],
+];
+
+foreach ($fixes as $model_id => $variables) {
+    $scope = new \Tancredi\Entity\Scope($model_id, $container->get('storage'), $container->get('logger'));
+    if(isset($scope->metadata['version']) && $scope->metadata['version'] >= 13) {
+        continue;
+    }
+    $scope->metadata['version'] = 13;
+    $scope->setVariables($variables);
+    $container->get('logger')->info("Update firmware template for NPX5v2");
+}
